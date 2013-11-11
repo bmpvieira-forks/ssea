* new interpolation function for fdr calculation * polished report metadata fields, corrected bug in sortable table

ssea/ssea/base.py
@@ -24,6 +24,7 @@ WEIGHT_METHODS = {'unweighted': WeightMethod.UNWEIGHTED,
                   'weighted': WeightMethod.WEIGHTED,
                   'exp': WeightMethod.EXP,
                   'log': WeightMethod.LOG}
+WEIGHT_METHOD_STR = dict((v,k) for k,v in WEIGHT_METHODS.iteritems())
 
 class NumpyJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -67,6 +68,24 @@ def quantile(a, frac, limit=(), interpolation_method='fraction'):
             raise ValueError("interpolation_method can only be 'fraction', " \
                              "'lower' or 'higher'")
     return score
+
+def interp(x, xp, yp):
+    '''
+    linear interpolation
+    x: x value to interpolate
+    xp: x coords of points
+    yp: y coords of points
+    returns y value corresponding to x
+    '''
+    b = np.searchsorted(xp, x, side='left')
+    if b == 0:
+        return yp[0]
+    if b >= xp.shape[0]:
+        return yp[-1]
+    a = b - 1
+    frac = (x - xp[a]) / (xp[b] - xp[a])
+    y = yp[a] + (yp[b] - yp[a]) * frac
+    return y
 
 def hist_quantile(hist, bins, frac, left=None, right=None):
     assert frac >= 0.0
@@ -190,8 +209,8 @@ class Config(object):
         log_func("name:                    %s" % (self.name))
         log_func("num processes:           %d" % (self.num_processes))
         log_func("permutations:            %d" % (self.perms))
-        log_func("weight method miss:      %s" % (self.weight_miss))
-        log_func("weight method hit:       %s" % (self.weight_hit))
+        log_func("weight method miss:      %s" % (WEIGHT_METHOD_STR[self.weight_miss]))
+        log_func("weight method hit:       %s" % (WEIGHT_METHOD_STR[self.weight_hit]))
         log_func("weight param:            %f" % (self.weight_param))
         log_func("output directory:        %s" % (self.output_dir))
         log_func("input matrix directory:  %s" % (self.matrix_dir))

ssea/ssea/test/test_base.py
@@ -9,7 +9,7 @@ import random
 import os
 import numpy as np
 
-from ssea.base import Metadata, SampleSet, chunk, hist_quantile
+from ssea.base import Metadata, SampleSet, chunk, hist_quantile, interp
 
 def generate_random_sample_sets(N, minsize, maxsize, samples):
     sample_sets = []
@@ -36,6 +36,17 @@ class TestBase(unittest.TestCase):
         self.assertEquals(list(chunk(4,4)), [(0, 1), (1, 2), (2, 3), (3, 4)])
         self.assertEquals(list(chunk(5,4)), [(0, 2), (2, 3), (3, 4), (4, 5)])
 
+    def test_interp(self):
+        x = np.random.normal(loc=0.5, scale=0.1, size=100000)
+        bins = np.linspace(0.0, 1.0, num=51)
+        h = np.histogram(x, bins=bins)[0]        
+        fp = np.zeros(len(bins), dtype=np.float)
+        fp[1:] = h.cumsum()
+        x = np.linspace(0,1,21)
+        y = np.interp(x, bins, fp) 
+        y2 = np.array([interp(v, bins, fp) for v in x])
+        self.assertTrue(np.array_equal(y, y2))
+        
     def test_hist_quantile(self):
         # TODO: write test cases
         #hist = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0,2.0,4.0,10.0,10.0,13.0,22.0,21.0,32.0,40.0,36.0,36.0,51.0,68.0,61.0,54.0,61.0,55.0,55.0,36.0,24.0,8.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])

ssea/ssea/test/test_performance.py
@@ -3,47 +3,47 @@ Created on Oct 24, 2013
 
 @author: mkiyer
 '''
-import unittest
-
 import numpy as np
 import sys
 
-from ssea.kernel import ssea_kernel
-from ssea.algo import transform_weights
+from ssea.base import interp
+#from ssea.kernel import ssea_kernel
+#from ssea.algo import transform_weights
 
-class TestPerformance(unittest.TestCase):
+def interp_setup(numbins):
+    import numpy as np
+    d = np.random.normal(loc=0.5, scale=0.1, size=100000)
+    bins = np.linspace(0.0, 1.0, num=numbins)
+    h = np.histogram(d, bins=bins)[0]        
+    fp = np.zeros(len(bins), dtype=np.float)
+    fp[1:] = h.cumsum()
+    x = np.linspace(0,1,num=100000)
+    return x, bins, fp
 
-    def test1(self):
-        # setup samples
-        nsamples = 10000
-        nsets = 100
-        iterations = 2000
-        # create membership array and fill with random values
-        membership = np.empty((nsamples,nsets), dtype=np.uint8)
-        membership[:] = np.random.random_integers(0, 1, (nsamples,nsets))
-        # create random weights (use integers to create ties)
-        weights = np.empty(nsamples, dtype=np.float)
-        weights[:] = np.sort(np.random.random_integers(-10, 10, nsamples))[::-1]
-        # transform weights based on weight method
-        weight_method = 'weighted'
-        weights_miss = np.fabs(transform_weights(weights, weight_method))
-        weights_hit = np.fabs(transform_weights(weights, weight_method))
-        # run kernel
-        perm = np.arange(nsamples)
-        for i in xrange(iterations):
-            np.random.shuffle(perm)
-            print i
-            es_vals, es_run_inds, es_runs = \
-                ssea_kernel(weights, weights_miss, weights_hit, membership, perm)
+def interp_test1():
+    x, bins, fp = interp_setup(10001)
+    for v in x:
+        y = np.interp(v, bins, fp) 
+
+def interp_test2():
+    x, bins, fp = interp_setup(10001)
+    for v in x:
+        y = interp(v, bins, fp) 
 
 if __name__ == "__main__":
-    import cProfile
-    import pstats
-    profile_filename = '_profile.bin'
-    cProfile.run('unittest.main()', profile_filename)
-    statsfile = open("profile_stats.txt", "wb")
-    p = pstats.Stats(profile_filename, stream=statsfile)
-    stats = p.strip_dirs().sort_stats('cumulative')
-    stats.print_stats()
-    statsfile.close()
-    sys.exit(0)
+    import timeit
+    print 'hello'
+    print timeit.timeit('interp_test1()', setup='from __main__ import interp_test1', number=1)
+    print 'hello'
+    print timeit.timeit('interp_test2()', setup='from __main__ import interp_test2', number=1)
+
+#     import cProfile
+#     import pstats
+#     profile_filename = '_profile.bin'
+#     cProfile.run('unittest.main()', profile_filename)
+#     statsfile = open("profile_stats.txt", "wb")
+#     p = pstats.Stats(profile_filename, stream=statsfile)
+#     stats = p.strip_dirs().sort_stats('cumulative')
+#     stats.print_stats()
+#     statsfile.close()
+#     sys.exit(0)