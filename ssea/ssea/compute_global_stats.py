d4cfc20afa7f20eaed3e5b07093527423085901d
mkiyer <mkiyer@umich.edu>
2013-11-26 01:57:59 -0500

    updates to global stats

--- ssea/ssea/compute_global_stats.py
+++ ssea/ssea/compute_global_stats.py
@@ -21,13 +21,19 @@ def main(argv=None):
     parser.add_argument('input_dir')
     parser.add_argument('output_json_file')
     args = parser.parse_args()
+    if not os.path.exists(args.input_dir):
+        parser.error('input directory %s not found' % (args.input_dir))
+    if os.path.exists(args.output_json_file):
+        parser.error('output json file %s exists' % (args.output_json_file))
+    input_dir = args.input_dir
     tmp_dir = os.path.join(args.input_dir, TMP_DIR)
+    if not os.path.exists(tmp_dir):
+        parser.error('temp directory %s not found within input directory %s' % (tmp_dir, input_dir))
     # divide matrix rows across processes
-    es_hists_file = os.path.join(args.input_dir, Config.OUTPUT_HISTS_FILE)
-    worker_json_files = []
+    es_hists_file = os.path.join(input_dir, Config.OUTPUT_HISTS_FILE)
     worker_json_stats_files = []
-    for i,filename in glob.glob(os.path.join(tmp_dir, '*.json')):
-        worker_json_files.append(filename)
+    worker_json_files = glob.glob(os.path.join(tmp_dir, '*.json'))
+    for i in xrange(len(worker_json_files)):
         worker_json_stats_files.append(os.path.join(tmp_dir, 'w%03d.stats.json' % (i)))
     compute_global_stats_parallel(es_hists_file, worker_json_files, worker_json_stats_files)
     # run a shell 'cat' command because it is fast
