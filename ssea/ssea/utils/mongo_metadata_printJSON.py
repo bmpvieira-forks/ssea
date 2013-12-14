79cacb5d51d033272a125e81c3d7cb8a37f91586
yashar.niknafs <yashar.niknafs@gmail.com>
2013-12-14 00:58:39 -0500

    added mongo import update

--- ssea/ssea/utils/mongo_metadata_printJSON.py
+++ ssea/ssea/utils/mongo_metadata_printJSON.py
@@ -97,24 +97,34 @@ class Metadata(object):
             
 def main():
     parser = argparse.ArgumentParser()            
-    parser.add_argument('--colmeta', dest='col_metadata_file',
-                        help='file containing metadata corresponding to each '
-                        'column of the weight matrix file')
-    parser.add_argument('--rowmeta', dest='row_metadata_file',
-                        help='file containing metadata corresponding to each '
-                        'row of the weight matrix file')
+#     parser.add_argument('--colmeta', dest='col_metadata_file',
+#                         help='file containing metadata corresponding to each '
+#                         'column of the weight matrix file')
+#     parser.add_argument('--rowmeta', dest='row_metadata_file',
+#                         help='file containing metadata corresponding to each '
+#                         'row of the weight matrix file')
+    parser.add_argument("-r", dest="row", 
+                        action="store_true", default=False, 
+                        help="Print row_meta JSONs")
+    parser.add_argument("-c", dest="col", 
+                        action="store_true", default=False, 
+                        help="Print col_meta JSONs")
     parser.add_argument('matrix_dir')
     args = parser.parse_args()
     # check command line args
-    if not os.path.exists(args.col_metadata_file):
+    matrix_dir = os.path.abspath(args.matrix_dir)
+    col_metadata_file = os.path.join(matrix_dir, 'colmeta.tsv')
+    row_metadata_file = os.path.join(matrix_dir, 'rowmeta.tsv')
+    
+    if not os.path.exists(col_metadata_file):
         parser.error("Column metadata file '%s' not found" % (args.col_metadata_file))
-    if not os.path.exists(args.row_metadata_file):
+    if not os.path.exists(row_metadata_file):
         parser.error("Row metadata file '%s' not found" % (args.row_metadata_file))
     if not os.path.exists(args.matrix_dir):
         parser.error('matrix path "%s" not found' % (args.matrix_dir))
-    matrix_dir = os.path.abspath(args.matrix_dir)
-    col_metadata_file = os.path.abspath(args.col_metadata_file)
-    row_metadata_file = os.path.abspath(args.row_metadata_file)
+
+#     col_metadata_file = os.path.abspath(args.col_metadata_file)
+#     row_metadata_file = os.path.abspath(args.row_metadata_file)
     # open matrix
     bm = BigCountMatrix.open(matrix_dir)
     if bm.size_factors is None:
@@ -125,12 +135,14 @@ def main():
     logging.info("Reading column metadata")
     col_metadata = list(Metadata.parse_tsv(col_metadata_file, bm.colnames))
     # pipe row metadata into mongoimport 
-    logging.debug("Importing row metadata")
-    for m in row_metadata:
-        print >>sys.stdout, m.to_json()
-    logging.debug("Importing column metadata")
-    for m in col_metadata:
-        print >>sys.stdout, m.to_json()
+    if args.row:
+        logging.debug("Importing row metadata")
+        for m in row_metadata:
+            print >>sys.stdout, m.to_json()
+    if args.col:
+        logging.debug("Importing column metadata")
+        for m in col_metadata:
+            print >>sys.stdout, m.to_json()
     # cleanup
     bm.close()
 
